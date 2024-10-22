55a302086dc53793f53b4dafc35e24f18266970d
liwenlu88 <2816842036@qq.com>
2024-10-22 21:16:48 +0800

    路由

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,28 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\OperationLog;
 use Illuminate\Support\Facades\Route;
 
-Route::post('auth/login', [\App\Http\Controllers\AuthController::class, 'login'])
-    ->middleware(\App\Http\Middleware\OperationLog::class);
+Route::post('auth/login', [AuthController::class, 'login'])
+    ->middleware(OperationLog::class);
 
 Route::middleware([
     'auth:sanctum',
-    \App\Http\Middleware\OperationLog::class // 操作日志
+    OperationLog::class // 操作日志
 ])->group(function () {
-    Route::post('auth/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('auth/logout', [AuthController::class, 'logout']);
 
     Route::middleware([
         \App\Http\Middleware\CheckPermission::class // 权限检查
     ])->group(function () {
         // 用户管理
-        Route::get('users/options', [\App\Http\Controllers\UserController::class, 'options']);
-        Route::resource('users', \App\Http\Controllers\UserController::class);
+        Route::get('users/options', [UserController::class, 'options']);
+        Route::resource('users', UserController::class);
 
         // 角色管理
-        Route::resource('roles', \App\Http\Controllers\RoleController::class);
+        Route::resource('roles', RoleController::class);
     });
 });
