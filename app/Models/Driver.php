0252e62213815670e9879073c91be3f471984ccd
liwenlu88 <2816842036@qq.com>
2024-10-23 20:56:42 +0800

    恢复默认

---
+++ app/Models/Driver.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use DateTimeInterface;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Driver extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'contact_tel',
+        'account',
+        'password',
+        'position_status_id',
+        'description',
+        'car_id',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+
+    protected function serializeDate(DateTimeInterface $date): string
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+}
